73f3da1b95656efcdac56590ae7374fc13d2340f
b5040831 <b5040831@my.shu.ac.uk>
2017-01-18 11:41:43 +0000

    done some work

---
+++ testApp/grails-app/domain/com/enrol/Course.groovy
@@ -0,0 +1,29 @@
+package com.enrol
+
+class Course {
+	String department
+	String title
+	String leader
+	String code
+	String description
+	String studyMode
+	Date startDate
+	Date endDate
+	int numberOfStudents
+	double tuitionFees
+
+    static constraints = {
+	department blank:false, nullable:false
+	title blank:false, nullable:false
+	leader blank:false, nullable:false
+	code blank:false, nullable:false
+	description blank:false, nullable:false, maxSize:5000, widget:'textarea'
+	studyMode blank:false, nullable:false, size:20..20
+	startDate blank:false, nullable:false
+	endDate blank:false, nullable:false
+	numberOfStudents blank:false, nullable:false, min:20, max:60
+	tuitionFees blank:false, nullable:false, scale:2
+
+
+    }
+}
